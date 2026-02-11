e7e5a3a68c335e49a614c725a3e3d41a0914937d
Nimai N. <nrnagireddy@stu.naperville203.org>
2026-02-11 13:16:43 -0600

    removing previous changes
    they were too buggy and I had to revert them.

--- my-react-app/src/pages/stats/Stats.jsx
+++ my-react-app/src/pages/stats/Stats.jsx
@@ -1,5 +1,11 @@
+"use client";
 import React, { useEffect, useState } from "react";
 import "./Stats.css";
+import Link from 'next/link';
+
+interface Stats {
+  ID: Number;
+}
 
 const Stats = () => {
   const [stats, setStats] = useState(null);
@@ -9,7 +15,7 @@ const Stats = () => {
     if (user?.googleId) {
       fetch(`https://deca.redhawks.us/api/get-stats?googleId=${user.googleId}`)
         .then(res => res.json())
-        .then(data => setStats(data))
+        .then(data => setStats(data)) // NOTES FOR NEXT TIME: IMPLEMENT STATS STUFF MORE 
         .catch(err => console.error("Stats fetch error:", err));
     }
   }, []);
@@ -25,7 +31,21 @@ const Stats = () => {
           <h1>Time Per Card: {stats.AvgTime?.toFixed(2)} sec</h1>
         </>
       ) : (
-        <h2>Coming Soon!</h2>
+        <table className="table-auto border-collapse border border-gray-400">
+          <thead>
+            <tr className="bg-blue-900 text-white">
+              <th className="border border-gray-300 px-4 py-2">ID</th>
+            </tr>
+          </thead>
+          <tbody>
+            {inventory.map((stats, index) => (
+              <tr key={index} className="odd:bg-gray-100 even:bg-blue-100">
+                <td className="border border-gray-300 px-4 py-2">{stats.ID}</td>
+        
+              </tr>
+            ))}
+          </tbody>
+        </table>
       )}
     </div>
   );
