58a7a838ba9be615028b64f560425c1ec81fcb33
Jonathan Yuan <142510555+JonathanYuan3@users.noreply.github.com>
2026-03-24 13:19:16 -0500

    Tests final for now

--- my-react-app/src/pages/Tests/Tests.jsx
+++ my-react-app/src/pages/Tests/Tests.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
+import './Tests.css';
 
-export default function Tests() {
+export default function Tests () {
     const [selectedFile, setSelectedFile] = useState(null);
 
     const predefinedFiles = [
         //ex: { id: x, name: 'Document x', url: 'https://drive.google.com/file/d/file_id/preview' }, replace file_id with actual file id from google drive
         { id: 1, name: 'Business Management & Administration', url: 'https://drive.google.com/file/d/1XkvwWt0N3ovx1rlQ3UxZlBum3E_7Q-PX/preview' },
-        { id: 2, name: 'Document 2', url: 'https://drive.google.com/file/d/file_id/preview' },
-        { id: 3, name: 'Document 3', url: 'https://drive.google.com/file/d/file_id/preview' },
-        { id: 4, name: 'Document 4', url: 'https://drive.google.com/file/d/file_id/preview' },
-        { id: 5, name: 'Document 5', url: 'https://drive.google.com/file/d/file_id/preview' },
+        { id: 2, name: 'Business Administration', url: 'https://drive.google.com/file/d/1gycSHrMxJQOMUxukyaHXn-q9mJS7e1bf/preview' },
+        { id: 3, name: 'Finance', url: 'https://drive.google.com/file/d/1XlPzPe75QszHsmWxxgCpeyPdzhhcwJI0/preview' },
+        { id: 4, name: 'Hospitality & Tourism', url: 'https://drive.google.com/file/d/1oezSfkH877er7EJRCARjY_vTDEVkiu5p/preview' },
+        { id: 5, name: 'Marketing', url: 'https://drive.google.com/file/d/1QZH6mnRsMiDND6e68jesAI74wSv4J26L/preview' },
     ];
 
     return (
@@ -27,7 +28,6 @@ export default function Tests() {
                     ))}
                 </div>
             </div>
-
             {selectedFile && (
                 <div className="file-viewer">
                     <h3>{selectedFile.name}</h3>
@@ -35,9 +35,16 @@ export default function Tests() {
                         src={selectedFile.url}
                         title={selectedFile.name}
                         className="file-embed"
-                        width={800}
-                        height={600}
                     />
+                </div>
+            )}
+            {selectedFile && (
+                <div>
+                    <h2>If you'd like to download this file, please click the download button below: </h2>
+                    
+                    <a href={selectedFile.url.replace('/preview', '/uc?export=download')} download>
+                        <button className="download-button">Download {selectedFile.name}</button>
+                    </a>
                 </div>
             )}
         </div>
