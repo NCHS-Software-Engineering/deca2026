4b23654b9bf0e86bcdeb8d3a0c4cf95336f2a43b
Dr. Derek Miller <83288861+MillerNCHS@users.noreply.github.com>
2026-05-19 14:40:04 -0500

    Fix environment variable name and update port number

--- my-react-app/src/Server/server.js
+++ my-react-app/src/Server/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: process.env.Host,
-  user: process.env.User, 
+  user: process.env.Username, 
   password: process.env.Password,
   database: 'deca',
   waitForConnections: true,
@@ -527,5 +527,5 @@ app.get('*', (req, res) => {
 
 
 app.listen(4000, () => {
-  console.log(`Server is running on port 3000.`);
+  console.log(`Server is running on port 4000.`);
 });
