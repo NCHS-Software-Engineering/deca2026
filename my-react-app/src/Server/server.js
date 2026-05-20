596fb5804a4c19391ecb2833434d75fc2222fede
Nimai N. <nrnagireddy@stu.naperville203.org>
2026-05-20 08:41:34 -0500

    Revert "Load environment variables from custom .env path"
    This reverts commit 023b34b4b3465382d7052863a5e204577b90ad9f.

--- my-react-app/src/Server/server.js
+++ my-react-app/src/Server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
 const app = express();
 app.use(cors());
